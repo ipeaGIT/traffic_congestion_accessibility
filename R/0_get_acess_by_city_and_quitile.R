86ea1b4f806f88fb7437bd32252bf227d6d911fa
rafapereirabr <rafa.pereira.br@gmail.com>
2025-05-09 09:34:54 -0300

    reorganize code main table and figures

---
+++ R/0_get_acess_by_city_and_quitile.R
@@ -0,0 +1,152 @@
+# goiania metro and not muni
+# update no texto figures 2 and 3
+
+library(aopdata)
+library(data.table)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(scales)
+library(accessibility)
+library(h3jsr)
+library(geobr)
+
+setDTthreads(percent = 100)
+
+cidades <- c("bel", "bho","bsb","cam","cgr","cur","duq","for","goi","gua","mac","man","nat","poa","rec","rio","sal","sgo","slz","spo")
+
+# get hex in goiania muni
+goi_poly <- geobr::read_municipality(year=2010) |>
+  filter(code_muni == '5208707')
+
+goi_hexes <- h3jsr::polygon_to_cells(
+  geometry = sf::st_geometry(goi_poly),
+  res = 9
+  )
+
+goi_hexes <- goi_hexes |> unlist() |> as.vector()
+
+get_acess_by_quitile <- function(cidade){
+
+  message(cidade)
+
+  # land use data
+  landuse <- aopdata::read_landuse(city=cidade, showProgress = FALSE) |>
+    dplyr::select(id_hex, P001, R002, T001) |>
+    rename("id" = id_hex)
+
+  # filter goi to main muni, and not the metro
+  if(cidade == 'goi'){
+    landuse <- filter(landuse, id %in% goi_hexes)
+  }
+
+  hex_with_pop <- landuse[P001 > 0]$id
+  hex_with_jobs <- landuse[T001 > 0]$id
+
+
+  # read free-flow ttm
+  free <- arrow::open_csv_dataset(paste0("../../data/congestionamento/04AM/OD_TI_", cidade, ".csv")) |>
+    dplyr::select(origin_hex, destination_hex, Total_Time_04AM) |>
+    rename("from_id" = origin_hex,
+           "to_id" = destination_hex) |>
+    filter(from_id %in% hex_with_pop,
+           to_id %in% hex_with_jobs) |>
+    collect()
+
+
+  # read peak ttm
+
+  peak <- arrow::open_csv_dataset(paste0("../../data/acesso_oport/output_ttmatrix_ti/OD_TI_", cidade, ".csv"))
+  colnames <- names(peak)
+
+  if (all(c("origin_hex", "destination_hex", "median_morning_peak") %in% colnames)) {
+
+    peak <- peak |>
+      dplyr::rename("from_id" = origin_hex,
+                    "to_id" = destination_hex) |>
+      dplyr::filter(from_id %in% hex_with_pop,
+                    to_id %in% hex_with_jobs) |>
+      dplyr::select(from_id, to_id, median_morning_peak) |>
+      dplyr::collect()
+
+  } else {
+
+    peak <- peak |>
+      dplyr::rename("from_id" = origin,
+                    "to_id" = destination) |>
+      dplyr::filter(from_id %in% hex_with_pop,
+                    to_id %in% hex_with_jobs) |>
+      dplyr::select(from_id, to_id, median_morning_peak) |>
+      dplyr::collect()
+  }
+
+  gc(T)
+
+
+  # calculate access
+  df_04AM <- accessibility::cumulative_interval(
+    travel_matrix = free,
+    land_use_data = landuse,
+    interval = c(15,45),
+    interval_increment = 1,
+    opportunity = "T001",
+    travel_cost = "Total_Time_04AM"
+  )
+
+
+  df_peak <- accessibility::cumulative_interval(
+    travel_matrix = peak,
+    land_use_data = landuse,
+    interval = c(15,45),
+    interval_increment = 1,
+    opportunity = "T001",
+    travel_cost = "median_morning_peak"
+  )
+
+  # rename access columns
+  df_04AM <- df_04AM |> rename("T001_04AM"= T001)
+  df_peak <- df_peak |> rename("T001_peak"= T001)
+
+
+  # join access estimates
+  df <- dplyr::left_join(df_04AM, df_peak, by="id")
+
+
+  pop <- landuse |>
+    filter(P001 >0) |>
+    dplyr::select(id, P001, R002)
+
+  acc <- dplyr::left_join(pop, df, by= "id") |>
+    filter(R002 >0)
+
+  acc[is.na(acc)] <- 0
+
+
+  acc <- acc |>
+    group_by(R002) |>
+    summarise(pop = sum(P001),
+              mean_CMATT_free = weighted.mean(T001_04AM, P001/1000),
+              mean_CMATT_peak = weighted.mean(T001_peak, P001/1000)
+    )
+
+  acc <- acc |>
+    rename("quintile" = R002)
+
+  acc <- acc |>
+    mutate(cid = cidade,
+           quintile = as.character(quintile))
+
+  gc(T)
+  return(acc)
+}
+
+
+
+resultado_final <- pbapply::pblapply(
+  X = cidades,
+  FUN = get_acess_by_quitile)|>
+  data.table::rbindlist()
+
+table(resultado_final$cid)
+
+saveRDS(resultado_final, './data/table_acess_by_quitile.rds')
--- R/Fig2_English.R
+++ R/Fig2_English.R
@@ -1,148 +1,68 @@
-
-
-library(accessibility)
-library(dplyr)
-library(data.table)
 library(aopdata)
+library(data.table)
+library(dplyr)
 library(tidyverse)
-library(mapview)
 library(ggplot2)
 library(scales)
-
-setDTthreads(percent = 100)
-
-cidades <- c("bel", "bho","bsb","cam","cgr","cur","duq","for","goi","gua","mac","man","nat","poa","rec","rio","sal","sgo","slz","spo")
-
-acesso <- function(cidade){
-  
-  setwd("//storage6/usuarios/Proj_acess_oport/git_diego/congestionamento/04AM")
-  
-  free <- fread(paste0("OD_TI_", cidade, ".csv")) %>%
-    dplyr::select(origin_hex, destination_hex, Total_Time_04AM) %>%
-    rename("from_id" = origin_hex,
-           "to_id" = destination_hex)
-  
-  setwd("C:/Users/b35143921880/Downloads/peak")
-  
-  peak <- fread(paste0("OD_TI_",cidade,"_final.csv")) %>%
-    filter(origin_hex %in% free$from_id,
-           destination_hex %in% free$to_id) %>%
-    dplyr::select(origin_hex, destination_hex, median_morning_peak) %>%
-    rename("from_id" = origin_hex,
-           "to_id" = destination_hex)
-  
-  landuse <- aopdata::read_landuse(city=cidade) %>% dplyr::select(id_hex, T001) %>% rename("id" = id_hex)
-  
-  df_04AM <- cumulative_interval(travel_matrix = free,
-                                 land_use_data = landuse,
-                                 interval = c(15,45),
-                                 interval_increment = 1,
-                                 opportunity = "T001",
-                                 travel_cost = "Total_Time_04AM")
-  
-  df_04AM <- df_04AM %>% rename("T001_04AM"= T001)
-  
-    
-  df_peak <- cumulative_interval(travel_matrix = peak,
-                                 land_use_data = landuse,
-                                 interval = c(15,45),
-                                 interval_increment = 1,
-                                 opportunity = "T001",
-                                 travel_cost = "median_morning_peak")
-
-  df_peak <- df_peak %>% rename("T001_peak"= T001)
-  
-  
-  df <- dplyr::left_join(df_04AM, df_peak, by="id")
-  
-  pop <- aopdata::read_population(city = cidade) %>% filter(P001 >0) %>% dplyr::select(id_hex,P001)
-  
-  
-  acc <- dplyr::left_join(pop, df, by=c("id_hex"="id"))
-  
-  acc[is.na(acc)] <- 0
-  
-  acc <- acc %>%
-    group_by() %>%
-    summarise(mean_CMATT_free = weighted.mean(T001_04AM, P001/1000),
-              mean_CMATT_peak = weighted.mean(T001_peak, P001/1000))
-  
-  acc_cast <- as.data.frame(t(acc)) 
-  acc_cast <-acc_cast %>%
-    mutate(medida = row.names(acc_cast),
-           cid = cidade) %>%
-    rename("Jobs" = V1)
-  
-  row.names(acc_cast) <- c(seq(1:nrow(acc_cast)))
-  
-  return(acc_cast)
-  
-  }
-
-
-for(i in (1:length(cidades))){
-  resultado <- acesso(cidades[i])
-  if(i==1){resultado_final <- resultado}
-  else(resultado_final <- rbind(resultado_final,resultado))
-}
-
-rm(resultado)
-
-resultado_final_v2 <- resultado_final 
-
-resultado_final_v2 <-resultado_final_v2 %>%
-  mutate(grupo = paste0(substring(medida, 12, nchar(medida))))
-# 
-# resultado_final_v2 <- resultado_final_v2 %>%
-#   mutate(concat = paste0(cid,"_", limite))
-
-resultado_final_v2_peak <- resultado_final_v2 %>%
-  filter(grupo == "peak")
-
-resultado_final_v2_free <- resultado_final_v2 %>%
-  filter(grupo == "free")
-
-resultado_final_v3 <- dplyr::left_join(resultado_final_v2_peak, resultado_final_v2_free, by="cid") %>%
-  mutate(razao = 1-(Jobs.x/Jobs.y))
-
-resultado_final_v3 <- resultado_final_v3 %>%
-  dplyr::select(medida.x,cid, razao)
-
-resultado_final_v3 <- resultado_final_v3 %>%
-  rename("medida" = medida.x)
-
-resultado_final_v3$cid <- recode_factor(resultado_final_v3$cid, "bel"="Belém",
-                                                                    "bho"="Belo Horizonte",
-                                                                    "bsb"="Brasília",
-                                                                    "cam"="Campinas",
-                                                                    "cgr"="Campo Grande",
-                                                                    "cur"="Curitiba",
-                                                                    "duq"="Duque de Caxias",
-                                                                    "for"="Fortaleza",
-                                                                    "goi"="Goiânia",
-                                                                    "gua"="Guarulhos",
-                                                                    "mac"="Maceió",
-                                                                    "man"="Manaus",
-                                                                    "nat"="Natal",
-                                                                    "poa"="Porto Alegre",
-                                                                    "rec"="Recife",
-                                                                    "rio"="Rio de Janeiro",
-                                                                    "sal"="Salvador",
-                                                                    "sgo"="São Gonçalo",
-                                                                    "slz"="São Luís",
-                                                                    "spo"="São Paulo")
-
+library(accessibility)
 
 options(scipen = 999)
 
-p <- ggplot(resultado_final_v3, aes(y=reorder(cid, razao), x=razao))+
+
+# figure 2 --------------------------------------------------------------
+
+resultado_final <- readRDS('./data/table_acess_by_quitile.rds')
+
+resultado_final$ratio <- 1-(resultado_final$mean_CMATT_peak/resultado_final$mean_CMATT_free)
+
+resultado_final$cid <- recode_factor(resultado_final$cid, "bel"="Belém",
+                                     "bho"="Belo Horizonte",
+                                     "bsb"="Brasília",
+                                     "cam"="Campinas",
+                                     "cgr"="Campo Grande",
+                                     "cur"="Curitiba",
+                                     "duq"="Duque de Caxias",
+                                     "for"="Fortaleza",
+                                     "goi"="Goiânia",
+                                     "gua"="Guarulhos",
+                                     "mac"="Maceió",
+                                     "man"="Manaus",
+                                     "nat"="Natal",
+                                     "poa"="Porto Alegre",
+                                     "rec"="Recife",
+                                     "rio"="Rio de Janeiro",
+                                     "sal"="Salvador",
+                                     "sgo"="São Gonçalo",
+                                     "slz"="São Luís",
+                                     "spo"="São Paulo")
+
+
+# resultado_final <- resultado_final |>
+#   mutate(Quintil = case_when(Quintil == "1" ~ "1 - pobre",
+#                              Quintil == "5" ~ "5 - rico",
+#                              Quintil %in% c("2", "3","4") ~ Quintil))
+
+
+# resultado medio por cidade
+# figura 2 e tabela 1
+resultado_cidade <- resultado_final[
+  ,
+  .(avg_congest = mean(ratio),
+    avg_peak = mean(mean_CMATT_peak),
+    avg_free = mean(mean_CMATT_free)
+    ),
+  by = cid][order(avg_congest),]
+
+resultado_cidade
+
+
+p <- ggplot(resultado_cidade, aes(y=reorder(cid, avg_congest), x=avg_congest))+
   geom_line(aes(group=cid), size=2, color="grey")+
   geom_point(aes(color= "black"), size=4, shape=16, stroke=2,show.legend = F)
 
-
-p + scale_color_manual(name="",
-                       values = "black",
-                       labels="")+
+figure2 <- p + scale_color_manual(name="",
+                                  values = "black",
+                                  labels="")+
   scale_y_discrete(expand = c(.02,0))+
   scale_x_continuous(labels = scales::percent)+
   #facet_wrap(~limite)+
@@ -161,6 +81,6 @@ p + scale_color_manual(name="",
         plot.subtitle = element_text(size=14, color = "darkslategrey", margin = margin(b = 25)),
         plot.caption = element_text(size = 8, margin = margin(t=10), color = "grey70", hjust = 0))
 
-
-ggsave(filename="C:/Users/b35143921880/Documents/Fig2_English.png", plot=ggplot2::last_plot(),
+figure2
+ggsave(figure2, filename="./figures/Fig2_English.png",
        dpi = 300, width = 25, height = 20, units = "cm")
