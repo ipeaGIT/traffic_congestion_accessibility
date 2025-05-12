3c1161bcbb4d6d2d346cc053a4d3223c571ee140
rafapereirabr <rafa.pereira.br@gmail.com>
2025-05-12 18:16:42 -0300

    figure 4

--- R/Fig4.R
+++ R/Fig4.R
@@ -11,7 +11,7 @@ library(hrbrthemes)
 library(ggnewscale)
 library(cowplot)
 library(purrr)
-library(ggsn)
+# library(ggsn)
 library(BAMMtools)
 library(stringi)
 library(ggspatial)
@@ -22,13 +22,13 @@ library(accessibility)
 
 
 pop <- function(cidade, legenda){
-  landuse <- aopdata::read_landuse(city=cidade, geometry = T) %>%
-    filter(P001>0)%>%
-    mutate(P001 = P001/1000) %>%
-    dplyr::select(id_hex, P001, geometry)%>%
+  landuse <- aopdata::read_landuse(city=cidade, geometry = T) |>
+    filter(P001>0)|>
+    mutate(P001 = P001/1000) |>
+    dplyr::select(id_hex, P001, geometry)|>
     st_transform(3857)
 
-  map_tiles <- readRDS(paste0("./Proj_acess_oport/data/acesso_oport/maptiles_crop/2019/mapbox/maptile_crop_mapbox_", cidade,"_2019.rds"))
+  map_tiles <- readRDS(paste0("../../data/acesso_oport/maptiles_crop/2019/mapbox/maptile_crop_mapbox_", cidade,"_2019.rds"))
 
   mapa_pop <- ggplot()+
     geom_raster(data = map_tiles, aes(x, y, fill = hex), alpha = 1) +
@@ -46,8 +46,8 @@ pop <- function(cidade, legenda){
     ) +
     annotation_scale(location = "br", width_hint = 0.2, pad_y = unit(0, "cm")) +
     coord_sf(datum=NA) +
-    labs(title = "Popula??o",
-         fill = "Habitantes\n(x1.000)") +
+    labs(title = "Population",
+         fill = "Population\n(x1.000)") +
     if(legenda == "sim"){
       theme(legend.title = element_text(size = 14, face="bold",family = "serif"), plot.title = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank(), rect = element_blank(), axis.title.y=element_blank(),axis.title.x=element_blank(), legend.position = "bottom")
     }else if(legenda == "nao"){
@@ -61,14 +61,14 @@ pop <- function(cidade, legenda){
 ############### pop por quintil ####################
 
 pop_quintil <- function(cidade, legenda){
-  landuse <- aopdata::read_landuse(city=cidade, geometry = T) %>%
-    filter(P001>0)%>%
-    mutate(quintil = as.character(R002)) %>%
-    dplyr::select(id_hex, quintil, geometry)%>%
+  landuse <- aopdata::read_landuse(city=cidade, geometry = T) |>
+    filter(P001>0)|>
+    mutate(quintil = as.character(R002)) |>
+    dplyr::select(id_hex, quintil, geometry)|>
     st_transform(3857)
 
 
-  map_tiles <- readRDS(paste0("./Proj_acess_oport/data/acesso_oport/maptiles_crop/2019/mapbox/maptile_crop_mapbox_", cidade,"_2019.rds"))
+  map_tiles <- readRDS(paste0("../../data/acesso_oport/maptiles_crop/2019/mapbox/maptile_crop_mapbox_", cidade,"_2019.rds"))
 
   mapa_pop <- ggplot()+
     geom_raster(data = map_tiles, aes(x, y, fill = hex), alpha = 1) +
@@ -85,7 +85,7 @@ pop_quintil <- function(cidade, legenda){
     annotation_scale(location = "br", width_hint = 0.2, pad_y = unit(0, "cm")) +
     coord_sf(datum=NA) +
     labs(title = "Popula??o",
-         fill = "Quintil") +
+         fill = "Quintile") +
     if(legenda == "sim"){
       theme(legend.title = element_text(size = 14, face="bold",family = "serif"), plot.title = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank(), rect = element_blank(), axis.title.y=element_blank(),axis.title.x=element_blank(), legend.position = "bottom")
     }else if(legenda == "nao"){
@@ -101,22 +101,22 @@ pop_quintil <- function(cidade, legenda){
 ############ Distribui??o dos Empregos #############
 
 emp <- function(cidade, legenda){
-  landuse <- aopdata::read_landuse(city=cidade, geometry = T) %>%
-    filter(T001>0)%>%
-    mutate(T001 = T001/1000)%>%
-    dplyr::select(id_hex, T001, geometry)%>%
+  landuse <- aopdata::read_landuse(city=cidade, geometry = T) |>
+    filter(T001>0)|>
+    mutate(T001 = T001/1000)|>
+    dplyr::select(id_hex, T001, geometry)|>
     st_transform(3857)
 
-  map_tiles <- readRDS(paste0("./Proj_acess_oport/data/acesso_oport/maptiles_crop/2019/mapbox/maptile_crop_mapbox_", cidade,"_2019.rds"))
+  map_tiles <- readRDS(paste0("../../data/acesso_oport/maptiles_crop/2019/mapbox/maptile_crop_mapbox_", cidade,"_2019.rds"))
 
   if(cidade!="spo"){
-    landuse <- landuse %>%
+    landuse <- landuse |>
       mutate(T001 = case_when(T001>4 ~ 4,
                               T001 <4 ~ T001))
     quebras = c(1,2,3,4)
     rotulos = c("1","2","3",">4")
 
-  }else{landuse <- landuse %>%
+  }else{landuse <- landuse |>
     mutate(T001 = case_when(T001>7 ~ 7,
                             T001 <7 ~ T001))
   quebras = c(2,4,6)
@@ -142,8 +142,8 @@ emp <- function(cidade, legenda){
     ) +
     annotation_scale(location = "br", width_hint = 0.2, pad_y = unit(0, "cm")) +
     coord_sf(datum=NA) +
-    labs(title = "Empregos",
-         fill = "Empregos\n(x1.000)") +
+    labs(title = "Jobs",
+         fill = "Jobs\n(x1.000)") +
     if(legenda == "sim"){
       theme(legend.title = element_text(size = 14, face="bold",family = "serif"), plot.title = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank(), rect = element_blank(), axis.title.y=element_blank(),axis.title.x=element_blank(), legend.position = "bottom")
     }else if(legenda == "nao"){
@@ -171,27 +171,27 @@ pop_quintil_spo <- pop_quintil("spo","sim")
 emp_spo <- emp("spo","sim")
 
 
-ggdraw(xlim = c(0,135), ylim = c(0,165))+
-  draw_plot(pop_cur, x=11, y=100, width = 45, height = 60)+
-  draw_plot(pop_quintil_cur, x=38.5, y=100, width = 45, height = 60)+
-  draw_plot(emp_cur, x=65.5, y=100, width = 45, height = 60)+
+fig4 <-  ggdraw(xlim = c(0,135), ylim = c(0,165))+
+            draw_plot(pop_cur, x=11, y=100, width = 45, height = 60)+
+            draw_plot(pop_quintil_cur, x=38.5, y=100, width = 45, height = 60)+
+            draw_plot(emp_cur, x=65.5, y=100, width = 45, height = 60)+
 
-  draw_plot(pop_for, x=11, y=60, width = 45, height = 40)+
-  draw_plot(pop_quintil_for, x=38.5, y=60, width = 45, height = 40)+
-  draw_plot(emp_for, x=65.5, y=60, width = 45, height = 40)+
+            draw_plot(pop_for, x=11, y=60, width = 45, height = 40)+
+            draw_plot(pop_quintil_for, x=38.5, y=60, width = 45, height = 40)+
+            draw_plot(emp_for, x=65.5, y=60, width = 45, height = 40)+
 
-  draw_plot(pop_spo, x=11, y=0, width = 45, height = 60)+
-  draw_plot(pop_quintil_spo, x=38.5, y=0, width = 45, height = 60)+
-  draw_plot(emp_spo, x=65.5, y=0, width = 45, height = 60)+
+            draw_plot(pop_spo, x=11, y=0, width = 45, height = 60)+
+            draw_plot(pop_quintil_spo, x=38.5, y=0, width = 45, height = 60)+
+            draw_plot(emp_spo, x=65.5, y=0, width = 45, height = 60)+
 
-  draw_label("Curitiba", size = 18, fontface = "bold", x=105, y=140,angle = 270,fontfamily = "serif")+
-  draw_label("Fortaleza", size = 18, fontface = "bold", x=105, y=85,angle = 270,fontfamily = "serif")+
-  draw_label("S?o Paulo", size = 18, fontface = "bold", x=105, y=35,angle = 270,fontfamily = "serif")+
-  draw_label("Popula??o", size = 18, fontface = "bold", x=35, y=162,fontfamily = "serif")+
-  draw_label("Renda", size = 18, fontface = "bold", x=60.5, y=162,fontfamily = "serif")+
-  draw_label("Empregos", size = 18, fontface = "bold", x=87.5, y=162,fontfamily = "serif")
+            draw_label("Curitiba", size = 18, fontface = "bold", x=105, y=140,angle = 270,fontfamily = "serif")+
+            draw_label("Fortaleza", size = 18, fontface = "bold", x=105, y=85,angle = 270,fontfamily = "serif")+
+            draw_label("São Paulo", size = 18, fontface = "bold", x=105, y=35,angle = 270,fontfamily = "serif")+
+            draw_label("Population", size = 18, fontface = "bold", x=35, y=162,fontfamily = "serif")+
+            draw_label("Income", size = 18, fontface = "bold", x=60.5, y=162,fontfamily = "serif")+
+            draw_label("Jobs", size = 18, fontface = "bold", x=87.5, y=162,fontfamily = "serif")
 
 
-ggsave2(filename="Fig5.png", plot=ggplot2::last_plot(),
+ggsave2(fig4, filename="./figures/Fig4_English.png",
         dpi = 300, width = 35, height = 30, units = "cm")
 
