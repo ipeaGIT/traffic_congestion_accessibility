e212ace31c53df1a375eb51282f78009d2e519dc
rafapereirabr <rafa.pereira.br@gmail.com>
2025-05-12 18:17:22 -0300

    figure 1

--- R/Fig1_English.R
+++ R/Fig1_English.R
@@ -9,7 +9,7 @@ library(data.table)
 color_palette <- viridisLite::mako(11)
 description_color <- 'grey40'
 cities_colors <- c(
-  'S?o Paulo' = color_palette[2],
+  'São Paulo' = color_palette[2],
   'Fortaleza' = color_palette[7],
   'Recife' = color_palette[8],
   'Salvador' = color_palette[5],
@@ -17,10 +17,10 @@ cities_colors <- c(
   'Rio de Janeiro' = color_palette[3],
   'Curitiba' = color_palette[10],
   'Porto Alegre' = color_palette[9],
-  'Bras?lia' = color_palette[4]
+  'Brasília' = color_palette[4]
 )
 
-cities_rank_acessibilidade <- fread("./data/resultado_acessibilidade.csv") |>
+cities_rank_acessibilidade <- fread("./data/resultado_acessibilidade.csv", encoding = 'Latin-1') |>
   mutate(ordem=1)
 
 cities_rank_censo <- fread("./data/resultado_censo2010.csv") |>
@@ -31,15 +31,15 @@ cities_rank_censo <- fread("./data/resultado_censo2010.csv") |>
                             COD_MUN == 2927408 ~ "Salvador",
                             COD_MUN == 3106200 ~ "Belo Horizonte",
                             COD_MUN == 3304557 ~ "Rio de Janeiro",
-                            COD_MUN == 3550308 ~ "S?o Paulo",
+                            COD_MUN == 3550308 ~ "São Paulo",
                             COD_MUN == 4106902 ~ "Curitiba",
                             COD_MUN == 4314902 ~ "Porto Alegre",
-                            COD_MUN == 5300108 ~ "Bras?lia"))
+                            COD_MUN == 5300108 ~ "Brasília"))
 
 cities_rank_censo <- cities_rank_censo |>
   select("rank","cidade","ordem")
 
-cities_rank_tomtom <- fread("./data/resultado_tomtom.csv") |>
+cities_rank_tomtom <- fread("./data/resultado_tomtom.csv", encoding = 'Latin-1') |>
   mutate(ordem=3)
 
 cities_rank_geral <- rbind(cities_rank_acessibilidade,cities_rank_censo,cities_rank_tomtom) #|>
@@ -67,7 +67,7 @@ bump_chart_basic <- cities_rank_geral |>
     'text',
     x = c(1, 2, 3),
     y = c(0.25, 0.25, 0.25),
-    label = c("Accessibility", "Travel Time","Traffic Speed"),
+    label = c("Accessibility", "Commuting time","Traffic Speed"),
     hjust = c(0, 0.5, 1),
     vjust = 1,
     size = 4,
@@ -89,11 +89,11 @@ bump_chart_basic <- cities_rank_geral |>
       face = 'bold',
       size = 20
     )
-  ) +
-  labs(
-    title = 'Congestion Ranking',
-    subtitle = 'Congestion measured through different methods'
   )
+  # + labs(
+  #   title = 'Congestion Ranking',
+  #   subtitle = 'Congestion measured through different methods'
+  #)
 
-ggsave(filename="../figures/Fig1_English.png", plot=ggplot2::last_plot(),
+ggsave(filename="./figures/Fig1_English.png", plot=ggplot2::last_plot(),
        dpi = 300, width = 25, height = 15, units = "cm")
